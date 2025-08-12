9001c4d9e0fdc23af1725c46a932b380f32c7df4
Otavio Gondim <otaviogondim31@gmail.com>
2025-08-12 00:01:08 -0300

    comeco do backend da tela de login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
 }
---
+++ app/src/main/java/com/example/quiz/auth/GerenciadorAuth.kt
@@ -0,0 +1,63 @@
+package com.example.quiz.auth
+import android.content.Context
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+object GerenciadorAuth {
+
+    private const val TAG = "GerenciadoAuth"
+
+    private const val COLECAO_USUARIOS = "Usuarios"
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val db: FirebaseFirestore = Firebase.firestore
+
+    fun getUsuarioAtual(): FirebaseUser?{
+        return auth.currentUser
+    }
+
+    /**
+     * Função para criar uma nova conta de usuário com e-mail e senha.
+     * Após o sucesso, salva as informações do usuário no Firestore.
+     * @param "email" O e-mail do usuário.
+     * @param "senha" A senha do usuário.
+     * @param "nomeUsuario" O nome de usuário escolhido.
+     * @return Result<FirebaseUser> Um objeto Result contendo o FirebaseUser em caso de sucesso ou uma exceção em caso de falha.
+     */
+    suspend fun cadastrarUsuario(email: String, senha: String, nomeUsuario: String): Result<FirebaseUser> {
+        return try{
+            val resultadoAuth = auth.createUserWithEmailAndPassword(email, senha).await()
+            val usuarioFirebase = resultadoAuth.user!!
+
+            //salvar informações do usuario
+            salvarPerfilUsuario(usuarioFirebase.uid, email, nomeUsuario)
+
+            Log.d(TAG,"Usuario criado com sucesso: ${usuarioFirebase.uid}")
+            Result.success(usuarioFirebase)
+        } catch (e: Exception){
+            Log.e(TAG,"Falha ao criar o usuario" ,e)
+            Result.failure(e)
+        }
+    }
+    /**
+     * Função para fazer login de um usuário existente com e-mail e senha.
+     * @param "email" O e-mail do usuário.
+     * @param "senha" A senha do usuário.
+     * @return Result<FirebaseUser> Um objeto Result contendo o FirebaseUser em caso de sucesso ou uma exceção em caso de falha.
+     */
+
+    suspend fun fazerLogin(email: String, senha: String): Result<FirebaseUser>{
+        return try{
+            val resultadoAuth = auth.signInWithEmailAndPassword(email, senha).await()
+            val usuarioFirebase = resultadoAuth.user!!
+            Log.d(TAG, "Login bem sucess")
+        }
+    }
+}
